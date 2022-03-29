68d39a12de511d262262b01e6c1dcb7c3c88d769
MarwanAhmed25 <marwan4125881@gmail.com>
2022-03-29 13:35:48 +0200

    create db

---
+++ backend/migrations/sqls/20220329110410-db-table-down.sql
@@ -0,0 +1,14 @@
+drop table order_product;
+drop table orders;
+drop table comment;
+drop table product_images;
+drop table product;
+drop table brand;
+drop table catogery;
+drop table users;
+
+
+
+
+
+
