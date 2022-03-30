4540e8462f7a10184cf2b3779ab18a25fb89fdcd
Jaber Ahmed <jaber33357@gmail.com>
2022-03-30 19:55:03 +0200

    first commit nabar branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import ProductOverview from './components/productOverview';
+import NavbarComp from './components/NavbarComp'
 /*   <NavBar/> */
 function App() {
   return (
     <Container className="app" style={{ padding: " 10px 10px" }}>
       <Routes>
         <Route exact path='/' element={<ProductOverview />} />
+        <Route exact path='/navbar' element={<NavbarComp />} />
       </Routes>
     </Container>
   );
