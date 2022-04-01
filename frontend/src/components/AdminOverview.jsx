59e96ec49c591d37abe7c3c6ac295fed41b7117f
Abd-ElRahman007 <abdelrhman123work@gmail.com>
2022-04-01 02:45:09 +0200

    add the inputs in admin overview

--- frontend/src/components/AdminOverview.jsx
+++ frontend/src/components/AdminOverview.jsx
@@ -1,8 +1,18 @@
-import { SimpleGrid, Skeleton, Container, Group, useMantineTheme } from '@mantine/core';
+import { SimpleGrid, Container, Group, useMantineTheme } from '@mantine/core';
+import InputCurrency from './adminOverviewComponent/InputCurrency';
+import InputDropdown from './adminOverviewComponent/InputDropdown';
+import InputStoke from './adminOverviewComponent/InputStoke';
+import InputText from './adminOverviewComponent/InputText';
+import InputTextArea from './adminOverviewComponent/InputTextArea';
 import PhotoImport from './adminOverviewComponent/PhotoImport';
 
+
 const importPhoto = (height) => <PhotoImport height={height} radius="md" />;
-const inputs = (height) => <Skeleton height={height} radius="md" animate={false} />;
+const inputText = (height, data) => <InputText data={data} height={height} radius="md" />;
+const inputTextArea = (height, data) => <InputTextArea data={data} height={height} radius="md" />;
+const inputDropdown = (height, data) => <InputDropdown data={data} height={height} radius="md" />;
+const inputCurrency = (height) => <InputCurrency height={height} radius="md" />;
+const inputStoke=(height,min,max)=><InputStoke height={height} min={min} max={max} radius="md" />;
 const BASE_HEIGHT = 360;
 const getSubHeight = (children, spacing) =>
   BASE_HEIGHT / children - spacing * ((children - 1) / children);
@@ -14,9 +24,12 @@ export default function AdminOverview() {
       <SimpleGrid cols={2} breakpoints={[{ maxWidth: 'xs', cols: 1 }]}>
         {importPhoto(BASE_HEIGHT)}
         <Group direction="column">
-          {inputs(getSubHeight(4, theme.spacing.md))}
-          {inputs(getSubHeight(4, theme.spacing.md))}
-          {inputs(getSubHeight(4, theme.spacing.md))}
+          {inputText(getSubHeight(4, theme.spacing.md),{label: 'Product Name', placeholder: 'Product Name'})}
+          {inputDropdown(getSubHeight(4, theme.spacing.md),{label: 'Brand', placeholder: 'brand', data: ['brand1','brand2','brand3','brand4','brand5']})}
+          {inputDropdown(getSubHeight(4, theme.spacing.md), { label: 'Category', placeholder: 'category', data: ['category1', 'category2', 'category3', 'category4', 'category5'] })}
+          {inputCurrency(getSubHeight(4, theme.spacing.md))}
+          {inputStoke(getSubHeight(4, theme.spacing.md),0,100)}
+          {inputTextArea(getSubHeight(4, theme.spacing.md), { label: 'Description', placeholder: 'Description' })}
         </Group>
       </SimpleGrid>
     </Container>
