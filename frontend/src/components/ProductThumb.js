b4e5624288ba615a34a68f8e40bed7c8aab94f76
Mohammad <moh.osama84x@gmail.com>
2022-04-03 01:11:22 +0200

    add & remove to cart with notifications

--- frontend/src/components/ProductThumb.js
+++ frontend/src/components/ProductThumb.js
@@ -1,121 +1,176 @@
 
 import { Card, Image, Text, Badge, Button, Group, useMantineTheme, ColorSchemeProvider } from '@mantine/core';
-import { useState , useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import AddremoveButtons from './AddremoveButtons';
-import { addToCart , removeFromCart} from '../redux/slices/cartSlice';
+import { addToCart, removeFromCart } from '../redux/slices/cartSlice';
 import { useDispatch } from 'react-redux';
 import { cartState } from "../redux/slices/cartSlice"
 import { useSelector } from "react-redux"
-
-
-
-
+import { showNotification } from '@mantine/notifications';
+import { ShoppingCartPlus , ShoppingCartX } from 'tabler-icons-react';
 
 export default function ProductThumb(props) {
- const  {id , name , main_image ,price} = props.product
- console.log(" , props" , props)
-    const theme = useMantineTheme();
+  const { id, name, main_image, price } = props.product
+  console.log(" , props", props)
+  const theme = useMantineTheme();
 
-    const [quantity, setQuantity] = useState(1)
-    const [currentQuantity, setCurrentQuantity] = useState(0)
+  const [quantity, setQuantity] = useState(1)
+  const [currentQuantity, setCurrentQuantity] = useState(0)
 
-    const cartItems = useSelector(cartState)
-   // console.log("cartItems" , cartItems ) 
- const quantityInCart= cartItems.filter((item)=> {
-         
-      return   id===item.id
-          
-    })
-    console.log("quantityInCart" , quantityInCart.quantity , "this id" , id) 
-    const thisQ = quantityInCart[0]
-    console.log("thisQ" , thisQ , "this id" , id) 
-    
-    const increaseQuantity=()=>{
-      // stock logic here 
-        const number =quantity+1
-        setQuantity(number)
+  const cartItems = useSelector(cartState)
+  // console.log("cartItems" , cartItems ) 
+  const quantityInCart = cartItems.filter((item) => {
 
-    }
+    return id === item.id
+
+  })
+  console.log("quantityInCart", quantityInCart.quantity, "this id", id)
+  const thisQ = quantityInCart[0]
+  console.log("thisQ", thisQ, "this id", id)
+
+  const increaseQuantity = () => {
+    // stock logic here 
+    const number = quantity + 1
+    setQuantity(number)
+
+  }
 
 
-    const decreaseQuantity=()=>{
-      if(currentQuantity>0){
-          if (quantity <= -currentQuantity)  
-          return; 
-
-      }
-      else 
-        if (quantity <= 1)   return
-        
-         const number =quantity-1
-         setQuantity(number)
+  const decreaseQuantity = () => {
+    if (currentQuantity > 0) {
+      if (quantity <= -currentQuantity)
+        return;
 
     }
+    else
+      if (quantity <= 1) return
 
-    const dispatch = useDispatch()
-   
+    const number = quantity - 1
+    setQuantity(number)
 
-   useEffect(() => {
-     if (thisQ)
-    setCurrentQuantity(thisQ.quantity)
+  }
 
-     return () => {
+  const dispatch = useDispatch()
+
+  const notZero = () => {
+    if (quantity === 0)
+      alert("dont enter zero")
+  }
+
+
+  const cartAddFunction = (id, name, main_image, price, quantity) => {
+    if (quantity === 0) {
+      showNotification(message())
+      return
+    }
+    dispatch(addToCart({ id, name, main_image, price, quantity }))
+    setQuantity(1)
+    showNotification(message())
+  }
+
+
+  const cartRemoveFunction = () => {
+
+    dispatch(removeFromCart({ id }))
+    setQuantity(1)
+    showNotification({
+       title: "Cart changed",
+        message: `${id} has been removed from the cart` ,
+        color: 'red',
+        icon: <ShoppingCartX />
+        // radius and other props can be added 
+        // inner stylings to title/message/etc  can be done 
+       })
+
+  }
+
+
+  const message = () => {
+    if (quantity === 0)
+      return { title: "invalid amount", message: "0 is not a valid quantity" }
+    if (currentQuantity === 0)
+      return { title: "Cart changed", message: `${id} has been added to the cart`,icon: <ShoppingCartPlus /> }
+
+    else if (currentQuantity > 0 && quantity < 0 && currentQuantity === -quantity)
+      return { title: "Cart changed", message: `${id} has been removed from the cart`,icon: <ShoppingCartX /> ,color:"red" }
+
+    else if (currentQuantity > 0 && quantity < 0)
+      return { title: "Cart changed", message: `${id} has been decreased by ${-quantity}` ,icon: <ShoppingCartX /> }
+
+    else if (currentQuantity > 0 && quantity > 0)
+      return { title: "Cart changed", message: `${id} has been increased by ${quantity}` ,icon: <ShoppingCartPlus /> }
+
+      
+
+
+  }
+
+
+  useEffect(() => {
+    if (thisQ)
+      setCurrentQuantity(thisQ.quantity)
+
+    return () => {
       setCurrentQuantity(0)
-     }
-   }, [cartItems,thisQ])
+    }
+  }, [cartItems, thisQ])
 
-   
 
-    return (
-        <div style={{ width: 340, margin: 'auto' }}>
-        <Card shadow="sm" p="lg">
-          <Card.Section>
-            <Image src={main_image}
-                   alt="Product" 
-                    radius={10}
-                   
-             />
-          </Card.Section>
-  
-          <Group position="apart" style={{ marginBottom: 5, marginTop: theme.spacing.sm }}>
-            <Text weight={500}>{name}</Text>
-            <Badge color="pink" variant="light" size="xl" >
-              {price}
-            </Badge>
-          </Group>
-  
-          {/* <Text size="sm" style={{ color: secondaryColor, lineHeight: 1.5 }}>
+
+  return (
+    <div style={{ width: 340, margin: 'auto' }}>
+      <Card shadow="sm" p="lg">
+        <Card.Section>
+          <Image src={main_image}
+            alt="Product"
+            radius={10}
+
+          />
+        </Card.Section>
+
+        <Group position="apart" style={{ marginBottom: 5, marginTop: theme.spacing.sm }}>
+          <Text weight={500}>{name}</Text>
+          <Badge color="pink" variant="light" size="xl" >
+            {price}
+          </Badge>
+        </Group>
+
+        {/* <Text size="sm" style={{ color: secondaryColor, lineHeight: 1.5 }}>
             With Fjord Tours you can explore more of the magical fjord landscapes with tours and
             activities on and around the fjords of Norway
           </Text> */}
-             <Group position="apart" style={{ marginBottom: 5, marginTop: theme.spacing.sm }}>
-          <Button variant="light" 
-                  color="blue" 
-                  style={{ marginTop: 14 }}
-                  onClick={()=>{
-                    dispatch(addToCart( {id , name , main_image , price , quantity} ))
-                    setQuantity(1)
-                    }}
-           >
-            Add to Cart 
+        <Group position="apart" style={{ marginBottom: 5, marginTop: theme.spacing.sm }}>
+          <Button variant="light"
+            color="blue"
+            style={{ marginTop: 14 }}
+            onClick={() => {
+              cartAddFunction(id, name, main_image, price, quantity)
+
+              /*  dispatch(addToCart( {id , name , main_image , price , quantity} ))
+               setQuantity(1)
+
+               showNotification({
+                 title: 'Default notification',
+                 message: message()  ,
+               }) */
+            }}
+          >
+            Add to Cart
           </Button>
-            <AddremoveButtons
-                increaseQuantity={increaseQuantity}
-                decreaseQuantity={decreaseQuantity}
-                quantity={quantity}
-                    />
+          <AddremoveButtons
+            increaseQuantity={increaseQuantity}
+            decreaseQuantity={decreaseQuantity}
+            quantity={quantity}
+          />
 
-                        {currentQuantity>0 && <button onClick={()=>{
-                                          dispatch(removeFromCart( {id} ))
-                                          setQuantity(1)
-                                          }}>
-                                            remove
-                                            </button>
+          {currentQuantity > 0 && 
+              <button onClick={() => {cartRemoveFunction()}}>
+                remove
+              </button>
+          }
 
-                    }   
-                   
-          </Group>
-        </Card>
-      </div>
-    )
+        </Group>
+      </Card>
+    </div>
+  )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
-import {Provider} from "react-redux"
+import { Provider } from "react-redux"
 import store from "./redux/store"
-
+import { MantineProvider } from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+
+      <MantineProvider>
+        <NotificationsProvider  position="top-right" >
+          <App />
+        </NotificationsProvider>
+      </MantineProvider>
+
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
