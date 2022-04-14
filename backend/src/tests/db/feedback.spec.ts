99c28bb1c7b7bb8cc7720786f83a7ba19259afd4
MarwanAhmed25 <marwan4125881@gmail.com>
2022-04-14 12:57:14 +0200

    db testing

--- backend/src/tests/db/feedback.spec.ts
+++ backend/src/tests/db/feedback.spec.ts
@@ -1,87 +1,106 @@
-// import { Catogery } from '../../models/catogery';
-// import {Comment,comment} from '../../models/feedback';
-// import {Product,product} from '../../models/products';
-// import {User,user} from '../../models/users';
+import { Catogery , catogery} from '../../models/catogery';
+import {Comment,comment} from '../../models/feedback';
+import {Product,product} from '../../models/products';
+import {User,user} from '../../models/users';
+import { Brand, brand } from '../../models/brand';
+const comment_ = new Comment();
+const product_ = new Product();
+const user_ = new User();
+const brand_ = new Brand();
+const category_ = new Catogery();
 
-// const comment_ = new Comment();
-// const product_ = new Product();
-// const user_ = new User();
-// const category_ = new Catogery();
+const u:user ={
+    email: 'marwanl@gmail.com',
+    password: 'mar',
+    status: ''
+};
+const b:brand = {
+    name: 'kkl'
+};
+const p:product ={
+    code: 'maom',
+    name: '',
+    image: '',
+    category_id: 1,
+    price: 0,
+    currency: '',
+    stock: 0,
+    brand_id: 1
+};
+const c:catogery ={
+    name: 'mmm'
+};
 
-// describe('Tests for Feedback model', ()=>{
+let res_user:user, res_product:product,res_category:catogery, res_brand:brand;
+
+describe('Tests for Feedback model', ()=>{
     
-    
-//     //index function
-//     it('test index to be define',()=>{
-//         expect(comment_.index).toBeDefined();
-//     });
-//     it('test index to equal',async()=>{
+    //index function
+    it('test index to be define',()=>{
+        expect(comment_.index).toBeDefined();
+    });
+    it('test index to equal',async()=>{
+        res_category = await category_.create(c);
+        res_brand = await brand_.create(b);
+        p.category_id = res_category.id as number;
+        p.brand_id = res_brand.id as number;
+        res_product = await product_.create(p);
+        res_user = await user_.create(u);
 
-//         // const u:user ={
-//         //     email:'marwanl@gmail.com',
-//         //     password:'mar'
-//         // };
-//         // const res_user = await user_.create(u);
-
-//         const p:product ={
-//             code:'maom'
-//         };
-//         const res_product = await product_.create(p);
-
-
-//         const res = await comment_.index(res_product.id as unknown as number);
+        const res = await comment_.index(res_product.id as unknown as number);
         
-//         expect(res.length).toEqual(0);
-//     });
+        expect(res.length).toEqual(0);
+    });
 
-//     //create function
-//     it('test create to be define',()=>{
-//         expect(comment_.create).toBeDefined();
-//     });
-//     it('test create to equal',async()=>{
-//         const c:comment={
-//             message:'sdjk',
-//             user_id:1,
-//             product_id:1
-//         };
-//         const res = await comment_.create(c);  
-//         expect(res.message).toEqual('sdjk');
-//     });
+    //create function
+    it('test create to be define',()=>{
+        expect(comment_.create).toBeDefined();
+    });
+    it('test create to equal',async()=>{
+        const c:comment={
+            message:'sdjk',
+            user_id:res_user.id as number,
+            product_id:res_product.id as number
+        };
+        const res = await comment_.create(c);  
+        expect(res.message).toEqual('sdjk');
+    });
 
-//     //show function
-//     it('test show to be define',()=>{
-//         expect(comment_.show).toBeDefined();
-//     });
-//     it('test show to equal',async()=>{        
-//         const res = await comment_.show(1,1);
-//         expect(res.id).toEqual(1);
-//     });
+    //show function
+    it('test show to be define',()=>{
+        expect(comment_.show).toBeDefined();
+    });
+    it('test show to equal',async()=>{        
+        const res = await comment_.show(res_product.id as number,1);
+        expect(res.id).toEqual(1);
+    });
 
-//     //update function
-//     it('test update to define',()=>{
-//         expect(comment_.update).toBeDefined();
-//     });
-//     it('test update to equal',async()=>{
-//         const comments = await comment_.index(1);
-//         console.log(comments);
+    //update function
+    it('test update to define',()=>{
+        expect(comment_.update).toBeDefined();
+    });
+    it('test update to equal',async()=>{
+        const c:comment={
+            id: 1,
+            message: 'sd44jk',
+            user_id: res_user.id as number,
+            product_id: res_product.id as number
+        };
+        const res=await comment_.update(c);
+        expect(res.message).toEqual('sd44jk');
+    });
+
+    //delete function
+    it('test delete to be define',()=>{
+        expect(comment_.delete).toBeDefined();
+    });
+
+    it('test delete to equal',async()=>{
+        const res = await comment_.delete(res_product.id as number,1);
         
-//         const c:comment={
-//             id:1,
-//             message:'sd44jk'
-//         };
-//         const res=await comment_.update(c);
-//         expect(res.message).toEqual('sd44jk');
-//     });
-
-//     //delete function
-//     it('test delete to be define',()=>{
-//         expect(comment_.delete).toBeDefined();
-//     });
-
-//     it('test delete to equal',async()=>{
-//         const res = await comment_.delete(1,1,1);
-//         expect(res).toEqual('deleted');
-//     });
+        expect(res).toEqual('deleted');
+        
+    });
     
     
-// });
+});
--- backend/src/tests/db/products.spec.ts
+++ backend/src/tests/db/products.spec.ts
@@ -1,88 +1,79 @@
-// import {Product,product} from '../../models/products';
+import {Product,product} from '../../models/products';
 
-// const product_ = new Product();
-// const p:product={
-//     id: 1,
-//     code: 'string',
-//     name: 'string',
-//     model: 'string',
-//     image: 'string',
-//     description: 'string',
-//     category_id: 1,
-//     price: 1,
-//     currency: 'string',
-//     vote_count: 20,
-//     vote_total: 20,
-//     stock: 20,
-//     brand_id: 1,
-// };
-// describe('Tests for Product model', ()=>{
+const product_ = new Product();
 
-//     //index function
-//     it('test index be define',()=>{
-//         expect(product_.index).toBeDefined();
-//     })
-//     it('test index to equal',async()=>{
-//         const res = await product_.index();
-//         expect(res.length).toEqual(1);
-//     })
+const p:product={
+    code: 'mm11',
+    name: '',
+    image: '',
+    category_id: 2,
+    price: 0,
+    currency: '',
+    stock: 0,
+    brand_id: 2
+};
 
-//     //create function
-//     it('test create be define',()=>{
-//         expect(product_.create).toBeDefined();
-//     })
-//     it('test create to equal',async()=>{
-//         const res = await product_.create(p);
-//         expect(res).toEqual('created');
-//     })
+describe('Tests for Product model', ()=>{
 
-//     //show function
-//     it('test show be define',()=>{
-//         expect(product_.show).toBeDefined();
-//     })
-//     it('test show to equal',async()=>{
-//         const res = await product_.show(1);
+    //index function
+    it('test index be define',()=>{
+        expect(product_.index).toBeDefined();
+    });
+    it('test index to equal',async()=>{
+        const res = await product_.index();
+        expect(res.length).toEqual(1);
+    });
+
+    //create function
+    it('test create be define',()=>{
+        expect(product_.create).toBeDefined();
+    });
+    it('test create to equal',async()=>{
+        const res = await product_.create(p);
+        expect(res.code).toEqual('mm11');
+    });
+
+    //show function
+    it('test show be define',()=>{
+        expect(product_.show).toBeDefined();
+    });
+    it('test show to equal',async()=>{
+        const res = await product_.show(1);
         
-//         expect(res).toEqual({ id: 1,code: 'string',name: 'string',model: 'string',image: 'string',description: 'string',category_id: 1,
-//             price: 1,
-//             currency: 'string',
-//             vote_count: 20,
-//             vote_total: 20,
-//             stock: 20,
-//             brand_id: 1,})
-//     })
-
-//     //update function
-//     it('test update be define',()=>{
-//         expect(product_.update).toBeDefined();
-//     })
-//     it('test update to equal',async()=>{
-//         const p_:product={
-//             id: 1,
-//             code: 'string',
-//             name: 'string',
-//             model: 'string',
-//             image: 'string',
-//             description: 'string',
-//             category_id: 1,
-//             price: 1,
-//             currency: 'string',
-//             vote_count: 20,
-//             vote_total: 20,
-//             stock: 20,
-//             brand_id: 1,
-//         };
-//         const res = await product_.update(p_);
-//         expect(res).toEqual('updated');
-//     })
-//     //delete function
-//     it('test delete be define',()=>{
-//         expect(product_.delete).toBeDefined();
-//     })
-//     it('test delete to equal',async()=>{
-//         const res = await product_.delete(2);
-//         expect(res).toEqual('deleted');
-//     })
+        expect(res.code).toEqual('maom');
+    });
+    //update function
+    it('test update be define',()=>{
+        expect(product_.update).toBeDefined();
+    });
+    it('test update to equal',async()=>{
+        const p_:product={
+            id: 1,
+            code: 'string',
+            name: 'string',
+            model: 'string',
+            image: 'string',
+            description: 'string',
+            category_id: 2,
+            price: 1,
+            currency: 'string',
+            vote_count: 20,
+            vote_total: 20,
+            stock: 20,
+            brand_id: 2,
+        };
+        const res = await product_.update(p_);
+        expect(res.code).toEqual('string');
+    });
+    //delete function
+    it('test delete be define',()=>{
+        expect(product_.delete).toBeDefined();
+    });
+    it('test delete to equal',async()=>{
+        const res = await product_.delete(1);
+       
+        expect(res).toEqual('deleted');
+    });
     
     
-// })
+});
