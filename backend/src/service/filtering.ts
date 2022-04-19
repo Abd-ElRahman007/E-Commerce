ea7bf94cc5db2aef2593c8bffcb48c36c077311f
MarwanAhmed25 <marwan4125881@gmail.com>
2022-04-19 21:20:25 +0200

    filtter updates+

--- backend/src/service/filtering.ts
+++ backend/src/service/filtering.ts
@@ -1,15 +1,15 @@
 
 //return the data filtered by name if contains
 function isTrue(name:string, filter_name:string){
+    //console.log(name);
+    
     if(name!=null){
         name = name.toLowerCase();        
         filter_name = filter_name.toLowerCase();
 
         if(name.includes(filter_name))
             return true;
-    }else
-        throw new Error('please enter the name of product...');
-
+    }
     return false;
 }
 
