45b8b2fb7ebc668ace97ffda9f24ee513628e586
MarwanAhmed25 <marwan4125881@gmail.com>
2022-04-21 06:54:13 +0200

    models testing done+

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -3,10 +3,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { dbhost, db_user, db_password, db_name } = process.env;
+const { db_host, db_user, db_password, db_name } = process.env;
 const { test_db_host, test_db_user, test_db_password, test_db_name } = process.env;
 
-const dev = process.env.dev||'test'; 
+const dev = process.env.dev||'dev'; 
 
 let Client:Pool ;
 //test db connection
@@ -25,7 +25,7 @@ if(dev === 'test'){
     console.log(dev);
 
     Client= new Pool({
-        host: dbhost,
+        host: db_host,
         database: db_name,
         user: db_user,
         password: db_password,
--- backend/src/tests/api/brand.spec.ts
+++ backend/src/tests/api/brand.spec.ts
@@ -1,5 +1,5 @@
-/* import supertest from 'supertest';
-import route from '../../handlars/brand';
+import supertest from 'supertest';
+import route from '../../index';
 import { brand } from '../../models/brand';
 
 import dotenv from 'dotenv';
@@ -10,7 +10,11 @@ const {admin_email,admin_password} = process.env;
 
 const api = supertest(route);
 
-
+const b:brand = {
+    name:'kfds',
+    description:'',
+};
+let res_brand:brand;
 describe('Brand handlars api test',()=>{
 
     it('Brand index route',async ()=>{
@@ -19,35 +23,30 @@ describe('Brand handlars api test',()=>{
         expect(res.status).toBe(200);
     });
 
-    it('Brand show route',async ()=>{
-        
-        const res = await api.get('/brands/1');
-        expect(res.status).toBe(200);
-    });
-/* 
     it('Brand create route',async ()=>{
         
-        const d:brand={
-            'name':'marwan',
-            'description':'ahmed'
-        };
-        const res = await api.post('/brands').send(d);
+        const res = await api.post('/brands').send(b);
+        res_brand = res.body;
+        expect(res.status).toBe(200);
+    });
+
+    it('Brand show route',async ()=>{
+        
+        const res = await api.get(`/brands/${res_brand.id}`);
         expect(res.status).toBe(200);
     });
 
     it('Brand update route',async ()=>{
-        const d:brand={
-            'name':'marwan', 'description':'ahmed'
-        };
-        const res = await api.patch('/brands/1').send({'name':'marwan', 'description':'ahmed','admin_password':admin_password,'admin_email':admin_email});
+        
+        const res = await api.patch(`/brands/${res_brand.id}`).send({'name':'marwan', 'description':'ahmed','admin_password':admin_password,'admin_email':admin_email});
         expect(res.status).toBe(200);
     });
 
 
     it('Brand delete route',async ()=>{
-        const res = await api.delete('/brands/1').send({'admin_password':admin_password,'admin_email':admin_email});
+        const res = await api.delete(`/brands/${res_brand.id}`).send({'admin_password':admin_password,'admin_email':admin_email});
         expect(res.status).toBe(200); 
         
     });
  
-}); */
+});
